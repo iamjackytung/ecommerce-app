bdf3b14b3c9f98af00748679c0d21d4fe50004e1
Jacky Tung <jackyyet@usc.edu>
2022-10-28 15:56:39 -0700

    card components created

--- frontend/src/components/ProductCarouselComponent.js
+++ frontend/src/components/ProductCarouselComponent.js
@@ -1,43 +1,56 @@
 import Carousel from "react-bootstrap/Carousel";
+import { LinkContainer } from "react-router-bootstrap";
 
 const ProductCarouselComponent = () => {
+  const cursorP = {
+    cursor: "pointer",
+  };
   return (
     <Carousel>
       <Carousel.Item>
         <img
+          crossOrigin="anonymous"
           className="d-block w-100"
+          style={{ height: "300px", objectFit: "cover" }}
+          // style={{ height: "300px" }}
           src="/images/carousel/carousel-1.png"
           alt="First slide"
         />
         <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          <LinkContainer style={cursorP} to="/product-details">
+            <h3>Bestseller in Laptops Category</h3>
+          </LinkContainer>
+          <p>MacBook Pro 15 3000 Laptop, 18 inch HD</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{ height: "300px", objectFit: "cover" }}
           src="/images/carousel/carousel-2.png"
           alt="Second slide"
         />
 
         <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <LinkContainer style={cursorP} to="/product-details">
+            <h3>Bestseller in Books Category</h3>
+          </LinkContainer>
+          <p>The Great Gatsby</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img
           className="d-block w-100"
+          style={{ height: "300px", objectFit: "cover" }}
           src="/images/carousel/carousel-3.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <LinkContainer style={cursorP} to="/product-details">
+            <h3>Bestseller in Cameras Category</h3>
+          </LinkContainer>
+          <p>Canon D36 has the fastest camera</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
