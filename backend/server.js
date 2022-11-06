bd06f4c1c07e82480c0570f744000d77ba726e0a
Jacky Tung <jackyyet@usc.edu>
2022-11-05 22:22:55 -0700

    product and review schema created

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,29 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+
 const apiRoutes = require("./routes/apiRoutes");
 
-// app.use((req, res, next) => {
-//   console.log("first middleware");
-//   next();
-// });
-
-// app.get("/", (req, res, next) => {
-//   console.log("second middleware");
-//   res.send("Hello World! 111111");
-//   next();
-// });
-
-// app.get("/two", (req, res, next) => {
-//   console.log("third middleware");
-//   res.send("Hello World 2!");
-//   next();
-// });
-
-// app.use((req, res) => {
-//   console.log("fourth middleware");
-// });
-
-app.get("/", (req, res) => {
-  console.log("synchronous code");
-  throw new Error("some error occured");
+app.get("/", async (req, res, next) => {
   res.json({ message: "API running..." });
 });
 
-app.get("/a", (req, res, next) => {
-  setTimeout(() => {
-    console.log("asynchronous code");
-    // throw new Error("some error occured");
-    next(new Error("some error occured"));
-  }, 1000);
-  // res.send("Hello World!");
-});
+// mongodb connection
+const connectDB = require("./config/db");
+connectDB();
 
 app.use("/api", apiRoutes);
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  next(error);
+});
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
