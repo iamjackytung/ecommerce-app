ad9b124d1851992a030f5c331d51b72ec1ca7900
Jacky Tung <jackyyet@usc.edu>
2022-11-08 10:13:31 -0800

    pagination feature updated

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,8 +1,10 @@
 const Product = require("../models/ProductModel");
 const recordsPerPage = require("../config/pagination");
 
-const getProducts = async (req, res) => {
+const getProducts = async (req, res, next) => {
   try {
+    const pageNum = Number(req.query.pageNum) || 1;
+    res.json(pageNum);
     const products = await Product.find({})
       .sort({ name: 1 })
       .limit(recordsPerPage);
