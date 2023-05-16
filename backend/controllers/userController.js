cf1ced3ed91cae0b1a0bab0969bf86d1c931ddf6
Jacky Tung <jackyyet@usc.edu>
2023-05-15 23:51:38 -0700

    cookies created with middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/UserModel");
-const { hashPassword } = require("../utils/hashPassword");
+const { hashPassword, comparePasswords } = require("../utils/hashPassword");
 const generateAuthToken = require("../utils/generateAuthToken");
 
 const getUsers = async (req, res, next) => {
@@ -18,7 +18,7 @@ const registerUser = async (req, res, next) => {
       return res.status(400).send("All inputs are required");
 
     const userExists = await User.findOne({ email });
-    if (userExists) return res.status(400).send({ error: "user exists" });
+    if (userExists) return res.status(400).send("user exists");
     else {
       const hashedPassword = hashPassword(password);
       const user = await User.create({
@@ -60,4 +60,51 @@ const registerUser = async (req, res, next) => {
   }
 };
 
-module.exports = { getUsers, registerUser };
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password, doNotLogout } = req.body;
+    if (!(email && password))
+      return res.status(400).send("All inputs are required");
+
+    const user = await User.findOne({ email });
+    if (user && comparePasswords(password, user.password)) {
+      // to do: compare passwords
+      let cookieParams = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+      };
+
+      if (doNotLogout)
+        cookieParams = { ...cookieParams, maxAge: 1000 * 60 * 60 * 24 * 7 }; // 1000=1ms
+
+      return res
+        .cookie(
+          "access_token",
+          generateAuthToken(
+            user._id,
+            user.name,
+            user.lastName,
+            user.email,
+            user.isAdmin
+          ),
+          cookieParams
+        )
+        .json({
+          success: "user logged in",
+          userLoggedIn: {
+            _id: user._id,
+            name: user.name,
+            lastName: user.lastName,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            doNotLogout,
+          },
+        });
+    } else return res.status(401).send("wrong credentials");
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { getUsers, registerUser, loginUser };
