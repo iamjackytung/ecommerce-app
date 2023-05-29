75197c2af9542627a24aa21a4c5316f90407e2f3
Jacky Tung <jackyyet@usc.edu>
2023-05-29 00:37:00 -0700

    ensure that products can only review once. Transactions are also introduced for reviews for commiting and aborting transactions, and starting/ending sessions

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -151,26 +151,41 @@ const getUserProfile = async (req, res, next) => {
 
 const writeReview = async (req, res, next) => {
   try {
+    const session = await Review.startSession();
     const { comment, rating } = req.body;
     if (!(comment && rating)) {
       return res.status(400).send("All inputs are required");
     }
     const ObjectId = require("mongodb").ObjectId;
     let reviewId = ObjectId();
-    await Review.create([
-      {
-        _id: reviewId,
-        comment: comment,
-        rating: Number(rating),
-        user: {
-          _id: req.user._id,
-          name: req.user.name + " " + req.user.lastName,
+    session.startTransaction();
+    await Review.create(
+      [
+        {
+          _id: reviewId,
+          comment: comment,
+          rating: Number(rating),
+          user: {
+            _id: req.user._id,
+            name: req.user.name + " " + req.user.lastName,
+          },
         },
-      },
-    ]);
-    const product = await Product.findById(req.params.productId).populate(
-      "reviews"
+      ],
+      { session: session }
     );
+    const product = await Product.findById(req.params.productId)
+      .populate("reviews")
+      .session(session);
+
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user._id.toString() === req.user._id.toString()
+    );
+    if (alreadyReviewed) {
+      await session.abortTransaction();
+      session.endSession();
+      return res.status(400).send("product already reviewed");
+    }
+
     let prc = [...product.reviews];
     prc.push({ rating: rating });
     product.reviews.push(reviewId);
@@ -185,8 +200,11 @@ const writeReview = async (req, res, next) => {
           .reduce((sum, item) => sum + item, 0) / product.reviews.length;
     }
     await product.save();
+    await session.commitTransaction();
+    session.endSession();
     res.send("review created");
   } catch (err) {
+    await session.abortTransaction();
     next(err);
   }
 };
